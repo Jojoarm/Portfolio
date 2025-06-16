09a2e318a75e87923ad690f6eb1f61e178335147
George Ofogba <ofogbageorge3@gmail.com>
2025-06-16 22:02:04 +0100

    button update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({ text, className, id }: ButtonProps) => {
       onClick={(e) => {
         e.preventDefault();
 
-        const target = document.getElementById('counter');
+        const target = document.getElementById('projects');
 
         if (target && id) {
           const offset = window.innerHeight * 0.15;
